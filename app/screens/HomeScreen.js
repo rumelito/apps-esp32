141ca1584b276994085871dcf14a9bd23db4782a
rumelito <rommel.montalbom@gmail.com>
2024-12-06 00:18:54 +0800

    edit, debug and designing my homescreen

--- app/screens/HomeScreen.js
+++ app/screens/HomeScreen.js
@@ -4,194 +4,123 @@ import {
   View,
   Text,
   TouchableOpacity,
-  TextInput,
-  FlatList,
   ImageBackground,
+  Animated,
 } from "react-native";
 import { useNavigation } from "@react-navigation/native";
-
+import { ref, onValue, set } from "firebase/database";
+import { rtdb } from "./../../firebaseConfig";
+import Ionicons from "react-native-vector-icons/Ionicons"; // Import Ionicons
 
 const HomeScreen = () => {
   const navigation = useNavigation();
-  const [todos, setTodos] = useState([]);
-  const [editingId, setEditingId] = useState(null);
-  const [editedTodo, setEditedTodo] = useState("");
+  const [Count, setValue] = useState(0);
+  
+  const [borderColorAnimation] = useState(new Animated.Value(0));
+  const colors = ["#ff69b4", "#ff0000", "#ffff00", "#00ff00", "#0000ff", "#4b0082", "#9400d3"];
 
   useEffect(() => {
-    const fetchTodos = async () => {
-      try {
-        const todosRef = collection(db, "todos");
-        const querySnapshot = await getDocs(todosRef);
-        const todos = [];
-        querySnapshot.forEach((doc) => {
-          todos.push({ ...doc.data(), id: doc.id });
-        });
-        setTodos(todos);
-      } catch (error) {
-        console.error("Error fetching documents:", error);
-      }
-    };
-    fetchTodos();
+    const valueRef = ref(rtdb, "Count");
+    return onValue(valueRef, (snapshot) => {
+      setValue(snapshot.val());
+    });
   }, []);
 
-  const handleCreate = async () => {
-    try {
-      const newDocRef = await addDoc(collection(db, "todos"), {
-        content: "",
-      });
-      setTodos([...todos, { id: newDocRef.id, content: "" }]);
-    } catch (error) {
-      console.error("Error creating document:", error);
-    }
+  useEffect(() => {
+    const animateBorderColor = () => {
+      Animated.loop(
+        Animated.timing(borderColorAnimation, {
+          toValue: colors.length - 1,
+          duration: 5000,
+          useNativeDriver: false,
+        }),
+      ).start();
+    };
+
+    animateBorderColor();
+  }, [borderColorAnimation]);
+
+  const borderColorInterpolate = borderColorAnimation.interpolate({
+    inputRange: Array.from({ length: colors.length }, (_, i) => i),
+    outputRange: colors,
+  });
+
+  const handlePlus = () => {
+    const valueRef = ref(rtdb, "Count");
+    set(valueRef, Count + 1);
   };
 
-  const handleUpdate = async (id) => {
-    try {
-      const todosRef = collection(db, "todos");
-      const docRef = doc(todosRef, id);
-      await updateDoc(docRef, {
-        content: editedTodo,
-      });
-      const updatedTodos = todos.map((todo) =>
-        todo.id === id ? { ...todo, content: editedTodo } : todo
-      );
-      setTodos(updatedTodos);
-      setEditingId(null);
-      setEditedTodo("");
-    } catch (error) {
-      console.error("Error updating document:", error);
-    }
+  const handleMinus = () => {
+    const valueRef = ref(rtdb, "Count");
+    set(valueRef, Count - 1);
   };
-
-  const handleDelete = async (id) => {
-    try {
-      const todosRef = collection(db, "todos");
-      await deleteDoc(doc(todosRef, id));
-      setTodos(todos.filter((todo) => todo.id !== id));
-    } catch (error) {
-      console.error("Error deleting document:", error);
-    }
-  };
-
-  const renderItem = ({ item }) => (
-    <View style={styles.todoItemContainer}>
-      {editingId === item.id ? (
-        <TextInput
-          style={styles.todoItemInput}
-          placeholder="Add Todo"
-          value={editedTodo}
-          onChangeText={setEditedTodo}
-        />
-      ) : (
-        <Text style={styles.todoItem}>{item.content}</Text>
-      )}
-      <View style={styles.todoItemButtonContainer}>
-        {editingId === item.id ? (
-          <TouchableOpacity
-            style={styles.todoItemButton}
-            onPress={() => handleUpdate(item.id)}
-          >
-            <Text style={styles.todoItemButtonText}>Save</Text>
-          </TouchableOpacity>
-        ) : (
-          <TouchableOpacity
-            style={styles.todoItemButton}
-            onPress={() => {
-              setEditingId(item.id);
-              setEditedTodo(item.content);
-            }}
-          >
-            <Text style={styles.todoItemButtonText}>Edit</Text>
-          </TouchableOpacity>
-        )}
-        <TouchableOpacity
-          style={styles.todoItemButton}
-          onPress={() => handleDelete(item.id)}
-        >
-          <Text style={styles.todoItemButtonText}>Delete</Text>
-        </TouchableOpacity>
-      </View>
-    </View>
-  );
 
   return (
-    <ImageBackground
-      style={styles.container}
-      source={require("../assets/images/")}
-    >
-      <View style={styles.listContainer}>
-        <TouchableOpacity style={styles.button} onPress={handleCreate}>
-          <Text style={styles.buttonText}>Create</Text>
-        </TouchableOpacity>
-        <FlatList
-          data={todos}
-          keyExtractor={(item) => item.id}
-          renderItem={renderItem}
-          contentContainerStyle={styles.todoListContainer}
-        />
-      </View>
+    <ImageBackground style={styles.container} source={require("../assets/images/Background.jpg")}>
+      <Animated.View style={[styles.countContainer, { borderColor: borderColorInterpolate }]}>
+        <Text style={styles.countText}>Counting Machine Iot</Text>
+        <Text style={styles.value}>{Count}</Text>
+        <View style={styles.buttonContainer}>
+          <TouchableOpacity style={styles.button} onPress={handlePlus}>
+            <Text style={styles.buttonText}>+</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.button} onPress={handleMinus}>
+            <Text style={styles.buttonText}>-</Text>
+          </TouchableOpacity>
+        </View>
+      </Animated.View>
+
       <View style={styles.navContainer}>
-        <TouchableOpacity
-          style={styles.button2}
-          onPress={() => navigation.navigate("AboutScreen")}
-        >
+        <TouchableOpacity style={styles.navButton} onPress={() => navigation.navigate("AboutScreen")}>
+          <Ionicons name="information-circle-outline" size={24} color="white" />
           <Text style={styles.buttonText}>About Us</Text>
         </TouchableOpacity>
-        <TouchableOpacity
-          style={styles.button2}
-          onPress={() => navigation.navigate("LoginScreen")}
-        >
+        <TouchableOpacity style={styles.navButton} onPress={() => navigation.navigate("LoginScreen")}>
+          <Ionicons name="log-out-outline" size={24} color="white" />
           <Text style={styles.buttonText}>Logout</Text>
         </TouchableOpacity>
       </View>
     </ImageBackground>
   );
-};
+}
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
   },
-  todoListContainer: {
+  countContainer: {
+    justifyContent: "center",
+    backgroundColor: "rgba(255, 255, 255, 0.5)", // Semi-transparent background
+    alignItems: "center",
     padding: 20,
-  },
-  todoItemContainer: {
-    padding: 10,
-    marginVertical: 10,
-    backgroundColor: "#fff",
-  },
-  todoItem: {
-    fontSize: 18,
-    color: "#000",
-  },
-  todoItemInput: {
-    fontSize: 18,
-    borderBottomWidth: 1,
-    borderColor: "#ccc",
-    marginBottom: 10,
-  },
-  todoItemButtonContainer: {
-    flexDirection: "row",
-    justifyContent: "space-between",
-  },
-  todoItemButton: {
-    backgroundColor: "#33cccc",
-    padding: 10,
     borderRadius: 10,
-    marginHorizontal: 5,
+    borderWidth: 2,
+    margin: 20,
+    elevation: 5,
   },
-  todoItemButtonText: {
+  countText: {
+    fontSize: 24,
+    color: "#336234",
     textAlign: "center",
   },
-  button: {
-    width: 300,
-    backgroundColor: "#33cccc",
-    padding: 20,
-    borderRadius: 10,
-    marginBottom: 10,
-    marginTop: 60,
+  navContainer: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    position: "absolute",
+    bottom: 0,
+    left: 0,
+    right: 0,
+    padding: 10,
+    backgroundColor: "rgba(44, 120, 108, 0.8)", // Semi-transparent background
   },
-  button2: {
+  navButton: {
+    flexDirection: "row",
+    alignItems: "center",
+    marginHorizontal: 20,
+  },
+  button: {
     backgroundColor: "#33cccc",
     padding: 20,
     borderRadius: 10,
@@ -199,27 +128,20 @@ const styles = StyleSheet.create({
     marginHorizontal: 10,
     width: 100,
   },
-  listContainer: {
-    justifyContent: "center",
-    alignItems: "center",
-  },
-  navContainer: {
-    flexDirection: "row",
-    justifyContent: "space-between",
-    alignItems: "center",
-    position: "absolute",
-    bottom: 0,
-    left: 0,
-    right: 0,
-    padding: 10,
-    backgroundColor: "#2c786c",
-  },
   buttonText: {
     textAlign: "center",
+    color: "white",
+  },
+  value: {
+    fontSize: 100,
+    color: "#434a2a",
+    textAlign: "center",
+  },
+  buttonContainer: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    marginTop: 20,
   },
 });
 
 export default HomeScreen;
-
-
-
