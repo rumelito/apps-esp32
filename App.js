48dfe47754aa19266ea40e8cb22a970d642b96c4
rumelito <rommel.montalbom@gmail.com>
2024-12-10 03:54:55 +0800

    16. update app.js and install some package

--- App.js
+++ App.js
@@ -1,19 +1,26 @@
 import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import LoginScreen from "./app/screens/LoginScreen";
-import AboutScreen from "./app/screens/AboutScreen";
-import HomeScreen from "./app/screens/HomeScreen";
-import SignupScreen from "./app/screens/SignupScreen";
+import GetStartedScreen from "./app/screens/GetStartedScreen.js";
+import LoginScreen from "./app/screens/LoginScreen.js";
+import AboutScreen from "./app/screens/AboutScreen.js";
+import HomeScreen from "./app/screens/HomeScreen.js";
+import SignupScreen from "./app/screens/SignupScreen.js";
+
 const Stack = createNativeStackNavigator();
 
 export default function App() {
-  return (
-    <NavigationContainer>
-      <Stack.Navigator
-        initialRouteName="LoginScreen"
-        screenOptions={{ headerShown: false }}
+
+
+      return (
+        <NavigationContainer>
+          <Stack.Navigator
+            initialRouteName="GetStarted" // Set initial route to GetStarted
+            screenOptions={{
+              headerShown: false, // Hide header for all screens
+            }}
       >
+         <Stack.Screen name="GetStarted" component={GetStartedScreen} />
         <Stack.Screen name="LoginScreen" component={LoginScreen} />
         <Stack.Screen name="SignupScreen" component={SignupScreen} />
         <Stack.Screen name="HomeScreen" component={HomeScreen} />
